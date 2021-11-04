adb5d31fc0a7bf42ee5b40bf66a710f5da5df5cf
stretler <sarahtretler@gmail.com>
2021-11-03 22:20:26 -0400

    Update main.js

--- projects/fanpage/main.js
+++ projects/fanpage/main.js
@@ -1,15 +1,19 @@
 
-
-let body = document.body;
-
-let button = document.querySelectorAll(".format");
-
-console.log(button)
+// let buttons = document.querySelectorAll(".format a");
 
 
-button.forEach(function(button) {
+// console.log(buttons)
+
+  let buttons = document.querySelectorAll(".format a");
+  let container = document.querySelector(".newbg");
+
+buttons.forEach(function(button) {
   button.addEventListener('click', function (event) {
-     button.classList.toggle("newcolor");
+    // Remove existing newcolor class from each button
+    buttons.forEach(function(thisButton) {
+      thisButton.classList.remove("newcolor");
+    });
+    // Add newcolor class to this particular button, which was just clicked
+    button.classList.toggle("newcolor");
   });
 });
-
